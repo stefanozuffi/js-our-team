b9d3763c7ed57f169e75d4822e68c3f3117295d9
stefanozuffi <stefano.zuffi06@outlook.com>
2025-07-31 12:21:58 +0200

    first layout, cards put inside with loop, no particular styling

---
+++ script.js
@@ -0,0 +1,75 @@
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png"
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png"
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png"
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png"
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png"
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png"
+  }
+];
+
+
+
+// Mettiamo tutte le card in pagina
+
+        // <div class="card flex-c col col-md-6 col-lg-4">
+        //     <img class="card-image" src="./img/female1.png" alt="">
+        //     <div class="card-body">
+        //         <h5>Name</h5>
+        //         <span>Job</span>
+        //         <br>
+        //         <a href="#">Mail</a>
+        //     </div>
+        // </div>
+
+for (let i = 0; i<teamMembers.length; i++) {
+
+  const thisMember = teamMembers[i]
+
+  const {name, role, email, img} = thisMember
+
+  const innerhtml = `
+    <div class="card flex-c col-sm-12 col-md-6 col-lg-4">
+      <img class="card-image" src="./${img}" alt="">
+      <div class="card-body">
+          <h5>${name}</h5>
+          <span>${role}</span>
+          <br>
+          <a href="#">${email}</a>
+      </div>
+    </div>
+  `
+
+  const containerEl = document.getElementById('cnt')
+  containerEl.insertAdjacentHTML('beforeend', innerhtml)
+  
+}
